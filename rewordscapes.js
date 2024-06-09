4373ef627d052e8872dd8573cf2fcaea49519442
elksie <elisahreed@gmail.com>
2024-06-08 19:35:34 -0700

    Update rewordscapes.js

--- rewordscapes.js
+++ rewordscapes.js
@@ -215,6 +215,7 @@ function findHint(line) {
             let length = data[0].meanings[ranMeaning].synonyms.length; //number of synonyms of word
             let ranNum = Math.floor(Math.random() * length); //random number between 0 and length - 1
             if (data[0].meanings[ranMeaning].synonyms[ranNum] != "undefined") {
+                console.log(data[0].meanings[ranMeaning].synonyms[ranNum]);
                 synString = "<li>" + data[0].meanings[ranMeaning].synonyms[ranNum] + "<br>";
             } else {
                 synString = "<li>No synonyms found.<br>";
